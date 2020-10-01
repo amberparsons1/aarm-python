bcf7104ab56a567388e9a04393274c791ea9d5cb
Amber Parsons <68738239+amberparsons1@users.noreply.github.com>
2020-10-01 05:56:56 +0100

    Read in event traces and produce 1st stack

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # aarm-python
+
+This tool implements the absolute arrival-time recovery method (AARM) as developed in Boyce et al. (2017)**[1]**. It is written in python based off the bash tool developed alongside the paper **[2]**.
+
+This python tool is only an **initial prototype** with currently limited implementation as detailed below.
+
+## Requirements
+* Python 3 (only tested in version 3.7.3 on a macOS system)
+* Dependencies in requirements.txt (install with `pip install -r /path/to/requirements.txt`)
+* SAC data files formatted as detailed in the manual accompanying the bash tool (Section 2)
+
+## Using the tool
+Navigate inside the directory containing the event directories (e.g EXAMPLE_DATA). Run script calc_arr_times.py.
+
+This will:
+1. Get the event directory paths
+1. For each event:
+   1. Great a prepared event stream ready to stack: each trace is read in, cut around their alignment point t0 to 60 s, and normalized.
+   1. Form 1st stack (nth-root method) and plot.
+
+Implementing creating the 2nd stack and interactive picking to calculate absolute arrival-times is yet to be done.
+
+
+## TODOs/Cautions of current implementation
+* Stacking is done using nth-root method. Phase-weight stacking is preferred and should work using the obspy Stream.stack method (replacing the current stack_type argument '('root', 2)' with '('pw', 4))'. However this seems to produce no stack data (unless pw order is 0 - corresponding to linear stacking). Root stacking is instead used in the meantime.
+* After preparing and cutting a trace around t0 ready for stacking (prep_trace), the number of points in the trace (npts) is not always the same for each trace within an event. A tolerance (npts_tol) is specified for stacking which cuts the traces to match the shortest's length, enabling stacking. If the number of points to cut is greater than the tolerance an error is thrown and processing of the particular event is not continued (the event being moved into bad_events). This should be fixed so a tolerance is not needed, the preparation steps having ensured each trace is the same length.
+
+
+### References
+1. Boyce et al. (2017), 'From Relative to Absolute Teleseismic Travel Times: The Absolute Arrival-Time Recovery Method (AARM)', _Bulletin of the Seismological Society of America_, 107(5), pp. 2511–2520.
+2. https://github.com/alistairboyce11/AARM
---
+++ calc_arr_times.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+import glob
+import matplotlib.pyplot as plt
+import obspy
+from obspy import read
+from obspy.core import Stream
+import os
+import shutil
+
+
+CWD = os.getcwd()
+BAD_EVENTS_PATH = f"{CWD}/bad_events"
+
+
+def main():
+    # Event directory paths
+    evdirs = [evdir for evdir in glob.glob(f"{CWD}/??????????????") if os.path.isdir(evdir)]
+
+    for event in evdirs:
+        print("***",event.split("/")[-1],"***")
+
+        # Get stream with event traces prepared for stacking
+        event_stream = prepared_event_stream(event)
+
+        # 1st stack
+        try:
+            stack1 = event_stream.copy().stack(stack_type=('root', 2), npts_tol=2)  # TODO: use pw stacking
+        except ValueError as e:  # Skip event if npts for each trace too different
+            print(f"Skipping event as bad: {e}")
+            shutil.move(event, BAD_EVENTS_PATH)
+            continue
+
+        stack1.plot()
+
+
+def prepared_event_stream(event_path):
+    """
+    Read in each trace of an event, prepare trace for stacking (cut around t0,
+    normalize), and return as prepared stream.
+
+    Args:
+        event_path: path to event directory
+    """
+    seislist = glob.glob(f"{event_path}/*HZ")
+    stream = Stream()
+
+    for sac_file in seislist:
+        st = read(sac_file)
+        # Ensure only 1 trace read from sac_file, skip file otherwise
+        if len(st) != 1:
+            print(f"{len(st)} traces in {sac_file}! Expected 1.")
+            continue
+        prep_trace(st[0])
+        stream += st
+
+    # stream.plot(size=(800,700))
+    return stream
+
+
+def prep_trace(tr):
+    """
+    Prepare trace for stacking.
+
+    Set sampling rate at 40Hz, cut around t0 to 60s, normalize.
+
+    Args:
+        tr: obspy trace object
+    """
+    tr.decimate(2)
+    tr.interpolate(sampling_rate=40.0)
+    tr.taper(max_percentage=0.3)
+
+    # Ensure scaling of each trace is equal so can be stacked
+    if tr.stats.calib != 1.0:
+        tr.stats.calib = 1.0
+        # del tr.stats.sac['scale']
+
+    cut_trace_around_t0(tr)
+    tr.normalize()
+    # tr.plot()
+
+
+def cut_trace_around_t0(tr, pre_t0=30, post_t0=30, pad=True):
+    """
+    Cut trace around t0
+
+    Args:
+        tr: obspy trace object
+        pre_t0: time in seconds before t0 to cut
+        post_t0: time in seconds after t0 to cut
+    """
+    t0 = tr.stats.sac["t0"]
+    b = tr.stats.sac["b"]
+    e = tr.stats.sac["e"]
+    t_start = tr.stats.starttime + t0 - b - pre_t0
+    t_end = tr.stats.endtime + t0 - e + post_t0
+    tr.trim(t_start, t_end, pad=pad)
+
+
+if __name__ == "__main__":
+    if not os.path.exists(BAD_EVENTS_PATH):
+        os.mkdir(BAD_EVENTS_PATH)
+
+    main()
+    print("Run finished.")
